70246ecf2940d6551de40009fbe158d34902692d
Kezy <Kezy2807@gmail.com>
2023-03-04 15:56:15 +0300

    add discord_cfg

--- main/main.py
+++ main/main.py
@@ -1,17 +1,30 @@
 import discord
 from cfg import discord_cfg
-
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+from discord.ext import commands
 
 
 intents = discord.Intents.default()
 intents.message_content = True
+client = discord.Client(intents=intents)
 
-client = MyClient(intents=intents)
+bot = commands.Bot(command_prefix='j/', intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"Бот {client.user} запущен")
+    print("---------------")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hello'):
+        await message.channel.send('Hello!')
+
+
+
+
 client.run(f"{discord_cfg.token}")

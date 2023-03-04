65595c1971b97f03786c0942f901653690dddfdd
Kezy <Kezy2807@gmail.com>
2023-03-04 17:41:11 +0300

    fix ерунды

--- main/commands.py
+++ main/commands.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from discord.utils import get
 from discord.ext.commands import Bot
 import asyncio
-import voice_recording
+# import voice_recording
 
 
 intents = discord.Intents.all()
@@ -20,11 +20,11 @@ bot = commands.Bot(
 
 
 @bot.slash_command(name='record', description='Manually records audio', guild_ids=[872819304754724884])
-async def manual_record(ctx):
-    await ctx.defer()
-    await start_record(ctx)
-    asyncio.sleep(5)
-    await stop_recording(ctx)
+# async def manual_record(ctx):
+#     await ctx.defer()
+#     await start_record(ctx)
+#     asyncio.sleep(5)
+#     await stop_recording(ctx)
 
 
 @bot.slash_command(name='hehe', guild_ids=[872819304754724884])
@@ -48,10 +48,10 @@ async def on_message(message):
         await message.channel.send('Hello!')
 
 
-@bot.command()
+@bot.slash_command(name="join",  guild_ids=[872819304754724884])
 async def join(ctx):
     print("d")
-    channel = ctx.message.author.voice.channel
+    channel = ctx.author
     print(channel)
     voice = get(bot.voice_clients, guild=ctx.guild)
 

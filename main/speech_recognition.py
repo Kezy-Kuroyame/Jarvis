72d855cb1ef7cf3dacb27c74116e0e7bbd969a7a
f1v3nt5 <thenexusmc2706@gmail.com>
2023-03-04 18:38:36 +0300

    added session auth

--- main/speech_recognition.py
+++ main/speech_recognition.py
@@ -1,5 +1,10 @@
+from speechkit import Session
 from speechkit import ShortAudioRecognition
 
+
+oauth_token = 'y0_AgAAAAAfq-BCAATuwQAAAADdsREaEr0PDjebRducWEdLlXWIST7IlnM'
+folder_id = 'b1guqdbqvjqtpd8koeq8'
+oauth_session = Session.from_yandex_passport_oauth_token(oauth_token, folder_id)
 recognizeShortAudio = ShortAudioRecognition(session)
 
 

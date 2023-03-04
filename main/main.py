5a61766645b80a59694a3a49d560e269890638b6
Kezy <Kezy2807@gmail.com>
2023-03-04 15:42:22 +0300

    add intents in discord

--- main/main.py
+++ main/main.py
@@ -1,20 +1,17 @@
 import discord
+from cfg import discord_cfg
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run('your token here')
+client = MyClient(intents=intents)
+client.run("MTA4MTU1MDk5MDcxODgwNDA0MA.GLT3YW.M62WL4DtAgjETPSZwU-6cWLqTttv91Ed2LjExg")
